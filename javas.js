18835f21051665ca0de7cd5005f24c9bcde31f40
Me542 <florreyvin11@gmail.com>
2025-08-05 23:08:26 +0800

    5nd

--- javas.js
+++ javas.js
@@ -1,11 +1,33 @@
+// const pages = [
+//   "index.html",
+//   "about.html",
+//   "service.html",
+//   "project.html",
+//   "contact.html"
+// ];
 
-    const sections = ["hero, about", "service", "project", "contact"];
+// document.body.style.minHeight = "200vh";
 
-    sections.forEach(id => {
-      fetch(`${id}.html`)
-        .then(res => res.text())
-        .then(html => {
-          document.getElementById(id).innerHTML = html;
-        });
-    });
- 
+// window.addEventListener("scroll", () => {
+//   const scrollY = window.scrollY;
+//   const viewportHeight = window.innerHeight;
+//   const fullHeight = document.documentElement.scrollHeight;
+//   const current = window.location.pathname.split("/").pop();
+//   const i = pages.indexOf(current);
+
+//   // Scroll down to next
+//   if (scrollY + viewportHeight >= fullHeight - 5) {
+//     const next = pages[i + 1];
+//     if (next) {
+//       window.location.href = next;
+//     }
+//   }
+
+//   // Scroll up to previous
+//   if (scrollY <= 0) {
+//     const prev = pages[i - 1];
+//     if (prev) {
+//       window.location.href = prev;
+//     }
+//   }
+// });
